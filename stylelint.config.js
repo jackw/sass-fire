c2967cc6dde6a4c9f0c5108ed3258beb9f898012
Jack Westbrook <jack.westbrook@gmail.com>
2019-07-28 17:01:56 +0200

    chore: clean dependancies configure sassdoc

--- stylelint.config.js
+++ stylelint.config.js
@@ -7,19 +7,6 @@ module.exports = {
     plugins: ['stylelint-prettier'],
     rules: {
         'scss/at-function-pattern': null,
-        'prettier/prettier': true,
-        // 'order/order': [
-        //     {
-        //         type: 'at-rule',
-        //         hasBlock: false
-        //     }
-        // ]
-        // 'max-nesting-depth': 5,
-        // 'color-named': ['never', { ignore: ['inside-function'] }],
-        // 'property-no-unknown': [true, { ignoreProperties: ['error', 'warn'] }],
-        // 'scss/dollar-variable-pattern': ['^cudl', { ignore: 'local' }],
-        // 'selector-class-pattern': null,
-        // 'scss/selector-no-redundant-nesting-selector': true,
-        // 'length-zero-no-unit': null
+        'prettier/prettier': true
     }
 };
