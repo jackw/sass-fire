ad5bdcf659d4a298ea0ae389dbb1fa3d10b51403
Jack Westbrook <jack.westbrook@gmail.com>
2020-05-13 19:27:22 +0200

    style: run prettier on js files

--- scripts/test.js
+++ scripts/test.js
@@ -2,10 +2,12 @@ const path = require('path');
 const sassTrue = require('sass-true');
 const glob = require('glob');
 
-describe('Sass', () => {
+describe('Sass Fire', () => {
     const sassTestFiles = glob.sync(
         path.resolve(process.cwd(), 'test/*.spec.scss')
     );
 
-    sassTestFiles.forEach(file => sassTrue.runSass({ file }, { describe, it }));
+    sassTestFiles.forEach((file) =>
+        sassTrue.runSass({ file }, { describe, it })
+    );
 });
